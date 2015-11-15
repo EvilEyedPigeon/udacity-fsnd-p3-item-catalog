04b30e8194a7db2d248d5bc4a39290725a469b6e
Pedro Tejada <pedro.tejada.314@gmail.com>
2015-11-15 15:02:02 -0700

    feat: Add catalog endpoint

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello/")
 def hello():
     return "Hello World!"
+
+@app.route("/")
+@app.route("/catalog/")
+def show_catalog():
+	items = ["Phone", "Book", "Blue pen", "Banana"]
+	return render_template("catalog.html", items = items)
+
 
 if __name__ == "__main__":
     app.secret_key = 'super_secret_key'
