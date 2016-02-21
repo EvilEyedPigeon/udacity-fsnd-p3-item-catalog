4aaaa03083a6060113660349c3d4fb4360c24f88
Pedro Tejada <pedro.tejada.314@gmail.com>
2016-02-20 22:01:37 -0700

    feat: Add error handlers for HTTP errors 404 and 500

--- vagrant/catalog/catalog/__init__.py
+++ vagrant/catalog/catalog/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import render_template
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,17 +10,30 @@ from database_setup import Base, User, Category, Item
 
 app = Flask(__name__)
 
+
 # Configuration
 app_dir = os.path.abspath(os.path.dirname(__file__))
 app.config['CSRF_SECRET_KEY'] = 'csfr_super_secret_key'
 app.config['UPLOAD_FOLDER'] = os.path.join(app_dir, 'uploads')
 app.config['ALLOWED_IMAGE_EXTENSIONS'] = set(['jpg', 'jpeg', 'png', 'gif'])
 
+
 # Connect to database and create database session
 engine = create_engine('sqlite:///item_catalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 db = DBSession()
+
+
+# Register HTTP error handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 # Import modules
 import catalog.models
@@ -28,6 +42,7 @@ import catalog.models
 from views.api import api
 from views.auth import auth
 from views.data import data
+
 app.register_blueprint(api)
 app.register_blueprint(auth)
 app.register_blueprint(data)
