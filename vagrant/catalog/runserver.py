3f5ad82d3f51bcbc931ae1d3cbf5ec6de046da13
Pedro Tejada <pedro.tejada.314@gmail.com>
2016-05-22 13:38:30 -0600

    fix: Add __name__ == '__main__' check before running the app in runserver.py

--- vagrant/catalog/runserver.py
+++ vagrant/catalog/runserver.py
@@ -1,5 +1,6 @@
 from catalog import app
 
 # Start the app
-app.debug = True
-app.run(host = '0.0.0.0', port = 5000)
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
